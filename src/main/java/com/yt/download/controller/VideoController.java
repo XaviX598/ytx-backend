81b6159966be340521241d3ed095fbed14cc9d10
Xavier <kevinjkevps4@gmail.com>
2026-01-30 17:26:01 -0500

    cros 1

--- src/main/java/com/yt/download/controller/VideoController.java
+++ src/main/java/com/yt/download/controller/VideoController.java
@@ -9,17 +9,12 @@ import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBo
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.*;
 import java.util.Map;
 import java.util.UUID;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins = {
-    "http://localhost:3000",
-    "https://ytx-download.vercel.app"
-  })
 public class VideoController {
 
     private final String YT_DLP_PATH = System.getenv().getOrDefault(
